08955e5f5002a49d0570d98fe33b66875adacadb
Vladislav Lugovoy <vlugovoi2018@gmail.com>
2026-05-20 14:05:20 +0500

    fix: исправлено

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -16,7 +16,7 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
         <NavLink
           to='/'
           className={({ isActive }) =>
-            isActive ? styles.link_active : styles.link
+            `${styles.link} ${isActive ? styles.link_active : ``}`
           }
         >
           <BurgerIcon type={'primary'} />
@@ -25,7 +25,7 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
         <NavLink
           to='/feed'
           className={({ isActive }) =>
-            isActive ? styles.link_active : styles.link
+            `${styles.link} ${isActive ? styles.link_active : ``}`
           }
         >
           <ListIcon type={'primary'} />
@@ -39,7 +39,7 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
         <NavLink
           to='/profile'
           className={({ isActive }) =>
-            isActive ? styles.link_active : styles.link
+            `${styles.link} ${isActive ? styles.link_active : ``}`
           }
         >
           <ProfileIcon type={'primary'} />
